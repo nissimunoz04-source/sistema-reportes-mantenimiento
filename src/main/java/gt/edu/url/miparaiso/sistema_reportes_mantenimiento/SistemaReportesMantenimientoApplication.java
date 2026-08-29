c9388dac45072a8b99051c052e1993065d7878a5
Mía Muñoz <nissimunoz04@gmail.com>
2026-08-28 23:33:06 -0600

    Agregar proyecto base Spring Boot

---
+++ src/main/java/gt/edu/url/miparaiso/sistema_reportes_mantenimiento/SistemaReportesMantenimientoApplication.java
@@ -0,0 +1,13 @@
+package gt.edu.url.miparaiso.sistema_reportes_mantenimiento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaReportesMantenimientoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaReportesMantenimientoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gt/edu/url/miparaiso/sistema_reportes_mantenimiento/SistemaReportesMantenimientoApplicationTests.java
@@ -0,0 +1,13 @@
+package gt.edu.url.miparaiso.sistema_reportes_mantenimiento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaReportesMantenimientoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
